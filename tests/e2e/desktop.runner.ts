bcb889094fcae00085ba18c39ef5e2499259ae78
vlad-dargel <u.darhel@aisnovations.com>
2024-01-17 19:08:05 +0100

    update for runner

--- tests/e2e/desktop.runner.ts
+++ tests/e2e/desktop.runner.ts
@@ -1,51 +1,49 @@
 import testcafe from 'testcafe';
 
-(async(): Promise<void> => {
-    await testcafe('localhost')
-        .then(t => {
-            return t
-                .createRunner()
-                .compilerOptions({
-                    'typescript': {
-                        configPath: 'tsconfig.testcafe.json',
-                        experimentalDecorators: true
-                    } })
-                .src((process.env.TEST_FILES || 'tests/electron/**/*.e2e.ts').split('\n'))
-                .browsers(['electron --cache --allow-insecure-localhost --ignore-certificate-errors'])
-                .screenshots({
-                    path: './report/screenshots/',
-                    takeOnFails: true,
-                    pathPattern: '${USERAGENT}/${DATE}_${TIME}/${FIXTURE}_${TEST}_${FILE_INDEX}.png'
-                })
-                .reporter([
-                    'spec',
-                    {
-                        name: 'xunit',
-                        output: './results/results.xml'
-                    },
-                    {
-                        name: 'json',
-                        output: './results/e2e.results.json'
-                    },
-                    {
-                        name: 'html',
-                        output: './report/report.html'
-                    }
-                ])
-                .run({
-                    skipJsErrors: true,
-                    browserInitTimeout: 60000,
-                    selectorTimeout: 5000,
-                    assertionTimeout: 5000,
-                    speed: 1,
-                    pageRequestTimeout: 8000,
-                });
-        })
-        .then((failedCount) => {
-            process.exit(failedCount);
-        })
-        .catch((e) => {
-            console.error(e);
-            process.exit(1);
-        });
+(async (): Promise<void> => {
+  try {
+    const failedCount = await (await testcafe('localhost'))
+      .createRunner()
+      .compilerOptions({
+        'typescript': {
+          configPath: 'tsconfig.testcafe.json',
+          experimentalDecorators: true
+        }
+      })
+      .src((process.env.TEST_FILES || 'tests/electron/**/*.e2e.ts').split('\n'))
+      .browsers(['electron'])
+      .screenshots({
+        path: './report/screenshots/',
+        takeOnFails: true,
+        pathPattern: '${USERAGENT}/${DATE}_${TIME}/${FIXTURE}_${TEST}_${FILE_INDEX}.png'
+      })
+      .reporter([
+        'spec',
+        {
+          name: 'xunit',
+          output: './results/results.xml'
+        },
+        {
+          name: 'json',
+          output: './results/e2e.results.json'
+        },
+        {
+          name: 'html',
+          output: './report/report.html'
+        }
+      ])
+      .run({
+        skipJsErrors: true,
+        browserInitTimeout: 60000,
+        selectorTimeout: 5000,
+        assertionTimeout: 5000,
+        speed: 1,
+        pageRequestTimeout: 8000,
+      });
+
+    process.exit(failedCount);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
 })();
